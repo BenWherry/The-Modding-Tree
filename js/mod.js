357774d1a4e64b2e059813964438cec92f460bb7
Ben <benjobenjowherry@gmail.com>
2020-11-16 19:42:39 +0000

    Update mod.js
    changed basic information

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
 	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	id: "the eating tree",
+	author: "Ben",
+	pointsName: "calories",
 	discordName: "",
 	discordLink: "",
 	changelogLink: "https://github.com/Acamaeda/The-Modding-Tree/blob/master/changelog.md",
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "the eating tree",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
