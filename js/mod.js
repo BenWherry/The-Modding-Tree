b79325a43a9c87037aa9b9659480691001442eff
Ben <benjobenjowherry@gmail.com>
2020-11-16 22:21:56 +0000

    added upgrade
    (doesn't do anything)

--- js/mod.js
+++ js/mod.js
@@ -45,7 +45,7 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
-	print('you have decided that you must eat everything')
+	'you have decided that you must eat everything'
 ]
 
 // Determines when the game "ends"
